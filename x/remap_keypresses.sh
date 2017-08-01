2bf526a8e3abf95d15a7274cdc671a6be9eb650f
tecfu <help@tecfu.com>
2017-08-01 15:55:34 -0700

    Fixed Chrome keypress redirection

--- x/remap_keypresses.sh
+++ x/remap_keypresses.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# Use xdotool to get the parent process of the active window
 W=`xdotool getactivewindow`
-S1=`xprop -id ${W} |awk '/WM_CLASS/{print $4}'`
-S2='"google-chrome"'
+S0=`xprop -id ${W} |awk '/WM_CLASS/{print $4}'`
+
+# Check if active window parent has "chrome" in name
+echo "$S0" | grep -ic 'chrome'
+if [ $? -eq 0 ];then
+  S1=1
+else
+  S1=0
+fi
 
 case "$1" in
   "j")
     #if C-j was passed as an argument AND 
     #the focused window is chrome browser, press C-b
     #otherwise do nothing
-    if [ $S1 = $S2 ]; then
+    if [ "$S1" = 1 ]; then
       xdotool getwindowfocus key --window %@ ctrl+b
     else
       xdotool getwindowfocus key --window %@ ctrl+j
@@ -19,7 +27,7 @@ case "$1" in
     #if C-k was passed as an argument AND 
     #the focused window is chrome browser, press C-m
     #otherwise do nothing
-    if [ $S1 = $S2 ]; then
+    if [ "$S1" = 1 ]; then
       xdotool getwindowfocus key --window %@ ctrl+m
     else
       #xdotool getwindowfocus type --window %@ ctrl+k
@@ -28,7 +36,7 @@ case "$1" in
     ;;
   *)
     #if some other argument, write to keypress.error.log
-    Time = date +"%T"
-    echo "$Time: Argument $1 not recognized by remap_keypresses.sh" >> error.log
+    Time=$(date +%T%s%n)
+    echo "$Time: Argument $1 not recognized by remap_keypresses.sh" >> remap_keypresses.error.log.txt
     ;;
 esac
